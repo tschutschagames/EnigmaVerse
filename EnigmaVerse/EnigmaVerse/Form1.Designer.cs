0fac2a9380775989ce6dd36185229b548a934719
tschutschagames <business.tschutschagames@gmail.com>
2024-05-12 10:59:23 +0200

    Add files via upload

---
+++ EnigmaVerse/EnigmaVerse/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace EnigmaVerse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.buttonShowKey = new System.Windows.Forms.Button();
+            this.buttonGenKey = new System.Windows.Forms.Button();
+            this.labelTSGames = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 25);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(290, 128);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.BackColor = System.Drawing.Color.LimeGreen;
+            this.labelInput.Location = new System.Drawing.Point(9, 9);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(34, 13);
+            this.labelInput.TabIndex = 1;
+            this.labelInput.Text = "Input:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.BackColor = System.Drawing.Color.LimeGreen;
+            this.labelOutput.Location = new System.Drawing.Point(9, 185);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(39, 13);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.Text = "Output";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 202);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(290, 128);
+            this.textBoxOutput.TabIndex = 3;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.BackColor = System.Drawing.Color.Red;
+            this.buttonEncrypt.Location = new System.Drawing.Point(12, 159);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 4;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = false;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.BackColor = System.Drawing.Color.Gold;
+            this.buttonDecrypt.Location = new System.Drawing.Point(93, 159);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecrypt.TabIndex = 5;
+            this.buttonDecrypt.Text = "Decrypt ";
+            this.buttonDecrypt.UseVisualStyleBackColor = false;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.Silver;
+            this.buttonClear.Location = new System.Drawing.Point(227, 159);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(308, 25);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(230, 20);
+            this.textBoxKey.TabIndex = 7;
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBoxKey_TextChanged);
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.BackColor = System.Drawing.Color.LimeGreen;
+            this.labelKey.Location = new System.Drawing.Point(308, 9);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(28, 13);
+            this.labelKey.TabIndex = 8;
+            this.labelKey.Text = "Key:";
+            // 
+            // buttonShowKey
+            // 
+            this.buttonShowKey.BackColor = System.Drawing.Color.White;
+            this.buttonShowKey.Location = new System.Drawing.Point(389, 51);
+            this.buttonShowKey.Name = "buttonShowKey";
+            this.buttonShowKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowKey.TabIndex = 9;
+            this.buttonShowKey.Text = "Show";
+            this.buttonShowKey.UseVisualStyleBackColor = false;
+            this.buttonShowKey.Click += new System.EventHandler(this.buttonShowKey_Click);
+            // 
+            // buttonGenKey
+            // 
+            this.buttonGenKey.BackColor = System.Drawing.Color.White;
+            this.buttonGenKey.Location = new System.Drawing.Point(308, 51);
+            this.buttonGenKey.Name = "buttonGenKey";
+            this.buttonGenKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenKey.TabIndex = 10;
+            this.buttonGenKey.Text = "Gen Key";
+            this.buttonGenKey.UseVisualStyleBackColor = false;
+            this.buttonGenKey.Click += new System.EventHandler(this.buttonGenKey_Click);
+            // 
+            // labelTSGames
+            // 
+            this.labelTSGames.AutoSize = true;
+            this.labelTSGames.Location = new System.Drawing.Point(438, 317);
+            this.labelTSGames.Name = "labelTSGames";
+            this.labelTSGames.Size = new System.Drawing.Size(101, 13);
+            this.labelTSGames.TabIndex = 11;
+            this.labelTSGames.Text = "@tschutschagames";
+            this.labelTSGames.Click += new System.EventHandler(this.labelTSGames_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.Color.White;
+            this.buttonOpen.Location = new System.Drawing.Point(470, 51);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 12;
+            this.buttonOpen.Text = "Open key";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.White;
+            this.buttonSave.Location = new System.Drawing.Point(470, 80);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 13;
+            this.buttonSave.Text = "Save key";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonPath
+            // 
+            this.buttonPath.BackColor = System.Drawing.Color.White;
+            this.buttonPath.Location = new System.Drawing.Point(470, 109);
+            this.buttonPath.Name = "buttonPath";
+            this.buttonPath.Size = new System.Drawing.Size(75, 23);
+            this.buttonPath.TabIndex = 14;
+            this.buttonPath.Text = "Path";
+            this.buttonPath.UseVisualStyleBackColor = false;
+            this.buttonPath.Click += new System.EventHandler(this.buttonPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(551, 336);
+            this.Controls.Add(this.buttonPath);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.labelTSGames);
+            this.Controls.Add(this.buttonGenKey);
+            this.Controls.Add(this.buttonShowKey);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.textBoxInput);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(567, 375);
+            this.MinimumSize = new System.Drawing.Size(567, 375);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EnigmaVerse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.Button buttonShowKey;
+        private System.Windows.Forms.Button buttonGenKey;
+        private System.Windows.Forms.Label labelTSGames;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonPath;
+    }
+}
+
---
+++ EnigmaVerse/EnigmaVerse/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EnigmaVerse
+{
+    public partial class Form1 : Form
+    {
+        private static readonly byte[] IV = new byte[16] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "EnigmaVerse " + ProductVersion;
+            textBoxKey.MaxLength = 32;
+            textBoxKey.UseSystemPasswordChar = true;
+
+            string enigmaVerseFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "EnigmaVerse");
+            if (!Directory.Exists(enigmaVerseFolderPath))
+            {
+                Directory.CreateDirectory(enigmaVerseFolderPath);
+            }
+        }
+
+        //UI
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            string plainText = textBoxInput.Text;
+            string key = textBoxKey.Text;
+            string encryptedText = Encrypt(plainText, key);
+            textBoxOutput.Text = encryptedText;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            string encryptedText = textBoxInput.Text;
+            string key = textBoxKey.Text;
+            string decryptedText = Decrypt(encryptedText, key);
+            textBoxOutput.Text = decryptedText;
+        }
+
+        private void buttonGenKey_Click(object sender, EventArgs e)
+        {
+            textBoxKey.Clear();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] keyBytes = new byte[16];
+                rng.GetBytes(keyBytes);
+                string randomKey = BitConverter.ToString(keyBytes).Replace("-", "").ToLower();
+                textBoxKey.Text = randomKey;
+            }
+        }
+
+        private void buttonShowKey_Click(object sender, EventArgs e)
+        {
+            if (textBoxKey.UseSystemPasswordChar == true)
+            {
+                textBoxKey.UseSystemPasswordChar = false;
+                buttonShowKey.Text = "Hide";
+            }
+            else
+            {
+                textBoxKey.UseSystemPasswordChar = true;
+                buttonShowKey.Text = "Show";
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Clear();
+            textBoxOutput.Clear();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (textBoxKey.Text != "")
+            {
+                try
+                {
+                    string enigmaVerseFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "EnigmaVerse");
+                    string fileName = $"EnigmaVerse_Key_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+                    string filePath = Path.Combine(enigmaVerseFolderPath, fileName);
+                    File.WriteAllText(filePath, textBoxKey.Text);
+                    MessageBox.Show("File has been saved", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch
+                {
+                    MessageBox.Show("File could not be saved", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Key is empty", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string enigmaVerseFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "EnigmaVerse");
+            openFileDialog.InitialDirectory = enigmaVerseFolderPath;
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileContent = File.ReadAllText(openFileDialog.FileName);
+                    textBoxKey.Text = fileContent;
+                }
+                catch
+                {
+                    MessageBox.Show($"Error loading file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonPath_Click(object sender, EventArgs e)
+        {
+            string enigmaVerseFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "EnigmaVerse");
+            System.Diagnostics.Process.Start(enigmaVerseFolderPath);
+        }
+
+        private void labelTSGames_Click(object sender, EventArgs e)
+        {
+            string url = "https://tschutscha-games.webador.at/";
+            System.Diagnostics.Process.Start(url);
+        }
+
+        //Methods
+
+        private string Encrypt(string plainText, string key)
+        {
+            if (textBoxKey.Text.Length == 32)
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                    aesAlg.IV = IV;
+
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(plainText);
+                            }
+                        }
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("The key must be at least 32 characters long", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+        }
+
+        private string Decrypt(string cipherText, string key)
+        {
+            if (textBoxKey.Text.Length == 32)
+            {
+                try
+                {
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                        aesAlg.IV = IV;
+
+                        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                        using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                        {
+                            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                            {
+                                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                                {
+                                    return srDecrypt.ReadToEnd();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Decryption error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The key must be at least 32 characters long", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+        }
+
+        private void textBoxKey_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxKey.TextLength == 32)
+            {
+                textBoxKey.BackColor = Color.Lime;
+            }
+            else
+            {
+                textBoxKey.BackColor = Color.Red;
+            }
+        }
+    }
+}
